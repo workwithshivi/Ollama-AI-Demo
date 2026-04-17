558b7bdcfb13c7c94a0250c9d9b48ecc16040236
Shivam Punia <42895980+workwithshivi@users.noreply.github.com>
2026-04-17 13:15:16 +0530

    Initial commit

---
+++ src/main/java/com/spring/ai/demo/ollamaaidemo/OllamaAiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.ai.demo.ollamaaidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OllamaAiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OllamaAiDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/ai/demo/ollamaaidemo/OllamaAiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.ai.demo.ollamaaidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OllamaAiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
